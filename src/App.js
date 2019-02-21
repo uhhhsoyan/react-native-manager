0b887aadb6e7cb402f92e72be8ac86e30e0e46f4
Eric Essoyan <eric.essoyan@gmail.com>
2019-02-20 17:05:38 -0800

    LoginForm with Redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component  {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyBoG7NjvEVaWo-vf7FNATco8NWLTfeZXUM',
+            authDomain: 'manager-f96dc.firebaseapp.com',
+            databaseURL: 'https://manager-f96dc.firebaseio.com',
+            projectId: 'manager-f96dc',
+            storageBucket: 'manager-f96dc.appspot.com',
+            messagingSenderId: '12006846567'
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+                <LoginForm />
             </Provider>
         )
     }
