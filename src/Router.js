f2b5c8b06737950ba5e38f07e6d370d0d8689f7a
Eric Essoyan <eric.essoyan@gmail.com>
2019-02-26 10:06:39 -0800

    employee form reducer

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
@@ -13,10 +14,15 @@ const RouterComponent = () => {
                 <Scene key="main">
                     <Scene 
                     rightTitle="Add"
-                    onRight={() => { console.log('RIGHT!!'); }}
+                    onRight={() => Actions.employeeCreate()}
                     key="employeeList"
                     component={EmployeeList}
                     title="Employees" 
+                    />
+                    <Scene
+                    key="employeeCreate"
+                    component={EmployeeCreate}
+                    title="Employee"
                     />
                 </Scene>
             </Scene>
