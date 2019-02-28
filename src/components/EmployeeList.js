db56c3f88ffaec00648673de3f13371cd223ec99
Eric Essoyan <eric.essoyan@gmail.com>
2019-02-28 14:10:11 -0800

    lodash

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,13 +1,30 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions/EmployeeActions';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
+        this.createDataSource(this.props);
     }
+
+    componentWillReceiveProps(nextProps) {
+        // next props are the next set of props that this component will be rendered with
+        // this.props is still the old set of props
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.DataSource = ds.cloneWithRows(employees);
+    }
+
     render() {
+        console.log(this.props);
         return (
             <View>
                 <Text>Employee List</Text>
@@ -21,4 +38,11 @@ class EmployeeList extends Component {
     }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid }; // outputs >> { shift: 'Monday', name: 'Eric', id: '1j2j34' }
+    });
+    return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
