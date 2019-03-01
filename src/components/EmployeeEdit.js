d402f38cdf2ac4ee1262578278fc5bd4c18b018e
Eric Essoyan <eric.essoyan@gmail.com>
2019-02-28 20:47:51 -0800

    updates

---
+++ src/components/EmployeeEdit.js
@@ -0,0 +1,39 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions'; // getting clever here!
+import { Card, CardSection, Button } from './common';
+
+class EmployeeEdit extends Component {
+    componentDidMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ prop, value });
+        });
+    }    
+    
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+        console.log(name, phone, shift);
+    }
+
+    render() {
+        return (
+            <Card>
+                <EmployeeForm {...this.props} />
+                <CardSection>
+                    <Button onPress={this.onButtonPress.bind(this)}>
+                        Save Changes
+                    </Button>
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+    return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,7 +5,7 @@ import { CardSection } from './common';
 
 class ListItem extends Component {
     onRowPress() {
-        Actions.employeeCreate({ employee: this.props.employee }); 
+        Actions.employeeEdit({ employee: this.props.employee.item }); 
         // ListItem called with employee as arg so we can access it throught this.props
     }
     
